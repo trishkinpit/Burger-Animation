60b1c9c0d8f5df4234467a55d5d69bc57165970d
Pit <peter2705@mail.ru>
2017-11-21 23:09:42 +1000

    Berger

---
+++ js/burger.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.icon').click(function(){
+		$('.icon').toggleClass('active');
+	})
+
+	$('.icon').click(function(){
+		$('.menu').toggleClass('menu_opened');
+	})
+
+	$('.icon').click(function(){
+		$('body').toggleClass('body_owerloyd');
+	})
+
+	$('.icon').click(function(){
+		$('.headhesive--stick').toggleClass('header_mobile');
+	})
+})
+
---
+++ js/js.js
@@ -0,0 +1,5 @@
+var options = {
+  offset: 50
+}
+
+var header =  new  Headhesive ( ' .header ', options );
